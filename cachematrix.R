3b9e2257f7d421ae04c0147c4e1afa7a7debac5d
dspain <dmspain@yahoo.com>
2014-04-23 22:12:20 -0700

    4-space indentation as per lectures

--- cachematrix.R
+++ cachematrix.R
@@ -4,31 +4,31 @@
 
 ## Creates a matrix object that can cache its inverse
 makeCacheMatrix <- function(x = matrix()) {
-  s <<- NULL
-  set <- function(y) {
-    x <<- y
     s <<- NULL
-  }
-  get <- function() x
-  setsolve <- function(solve) s <<- solve
-  getsolve <- function() s
-  list(set = set, get = get,
-       setsolve = setsolve,
-       getsolve = getsolve)
+    set <- function(y) {
+      x <<- y
+      s <<- NULL
+    }
+    get <- function() x
+    setsolve <- function(solve) s <<- solve
+    getsolve <- function() s
+    list(set = set, get = get,
+         setsolve = setsolve,
+         getsolve = getsolve)
 }
 
 
 ## Computes the inverse of the matrix returned by makeCacheMatrix.
 ##  Retrieves the inverse from cache if it has already been calculated.
 cacheSolve <- function(x, ...) {
-  ## Return a matrix that is the inverse of 'x'
-  s <- x$getsolve()
-  if(!is.null(s)) {
-    message("getting cached data")
-    return(s)
-  }
-  data <- x$get()
-  s <- solve(data, ...)
-  x$setsolve(s)
-  s
+    ## Return a matrix that is the inverse of 'x'
+    s <- x$getsolve()
+    if(!is.null(s)) {
+      message("getting cached data")
+      return(s)
+    }
+    data <- x$get()
+    s <- solve(data, ...)
+    x$setsolve(s)
+    s
 }
